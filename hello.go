d33efb8a6c2c7d4c227853c74e8ee0962e99269f
hkaino <hidetoshi.kaino@oracle.com>
2022-06-23 12:16:40 +0000

    test

--- hello.go
+++ hello.go
@@ -9,6 +9,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, World")
 }
 
+
 func main() {
   http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
   http.ListenAndServe(":8080", nil)
